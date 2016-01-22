d2044c711a9d572a81b5ea2d8cd8a1759502c348
Jon Araquistain <jon@eero.com>
2016-01-22 13:30:24 -0800

    add vertical 1/3

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-(function() {
+(function () {
   var React = typeof window !== 'undefined' && window.React || require('react');
 
   var RATE_LIMIT = 25;
 
   var ComponentVisibilityMixin = {
-    setComponentVisbilityRateLimit: function(milliseconds) {
+    setComponentVisbilityRateLimit: function (milliseconds) {
       RATE_LIMIT = milliseconds;
     },
 
-    getInitialState: function() {
-      return { visible: false };
+    getInitialState: function () {
+      return {visible: false};
     },
 
-    componentDidMount: function() {
+    componentDidMount: function () {
       this.enableVisbilityHandling(true);
     },
 
-    componentWillUnmount: function() {
+    componentWillUnmount: function () {
       this.disableVisbilityHandling();
     },
 
@@ -26,30 +26,35 @@
      * well as whether or not it's invisible due to CSS rules based
      * on opacity:0 or visibility:hidden.
      */
-    checkComponentVisibility: function() {
+    checkComponentVisibility: function () {
       var domnode = this._dom_node,
-          gcs = window.getComputedStyle(domnode, false),
-          dims = domnode.getBoundingClientRect(),
-          h = window.innerHeight,
-          w = window.innerWidth,
-          // are we vertically visible?
-          topVisible = 0 < dims.top && dims.top < h,
-          bottomVisible = 0 < dims.bottom && dims.bottom < h,
-          verticallyVisible = topVisible || bottomVisible,
-          // also, are we horizontally visible?
-          leftVisible = 0 < dims.left && dims.left < w,
-          rightVisible = 0 < dims.right && dims.right < w,
-          horizontallyVisible = leftVisible || rightVisible,
-          // we're only visible if both of those are true.
-          visible = horizontallyVisible && verticallyVisible,
-          // lets determine element's position relative to viewport center (regardless of visibility)
-          topHalf = (h / 2) > dims.top + ((dims.bottom - dims.top) / 2),
-          leftHalf = (w / 2) > dims.left + ((dims.right - dims.left) / 2),
-          visibilityChanged, vPositionChanged, hPositionChanged;
+        gcs = window.getComputedStyle(domnode, false),
+        dims = domnode.getBoundingClientRect(),
+        h = window.innerHeight,
+        w = window.innerWidth,
+        elVCenter = dims.top + ((dims.bottom - dims.top) / 2),
+        elHCenter = dims.left + ((dims.right - dims.left) / 2),
+      // are we vertically visible?
+        topVisible = 0 < dims.top && dims.top < h,
+        bottomVisible = 0 < dims.bottom && dims.bottom < h,
+        verticallyVisible = topVisible || bottomVisible,
+      // also, are we horizontally visible?
+        leftVisible = 0 < dims.left && dims.left < w,
+        rightVisible = 0 < dims.right && dims.right < w,
+        horizontallyVisible = leftVisible || rightVisible,
+      // we're only visible if both of those are true.
+        visible = horizontallyVisible && verticallyVisible,
+      // lets determine element's position relative to viewport center (regardless of visibility)
+        topHalf = (h / 2) > elVCenter,
+        oneThird = (h / 3) > elVCenter,
+        twoThird = (h / 3) <= elVCenter < (h * 2 / 3),
+        threeThird = elVCenter >= (h * 2 / 3),
+        leftHalf = (w / 2) > elHCenter,
+        visibilityChanged, vPositionChanged, hPositionChanged;
 
       // but let's be fair: if we're opacity: 0 or
       // visibility: hidden, or browser window is minimized we're not visible at all.
-      if(visible) {
+      if (visible) {
         var isDocHidden = document.hidden;
         var isElementNotDisplayed = (gcs.getPropertyValue("display") === "none");
         var elementHasZeroOpacity = (gcs.getPropertyValue("opacity") === 0);
@@ -59,20 +64,23 @@
 
       //Track what changed so the callback can can react appropriately
       visibilityChanged = visible !== this.state.visible;
-      vPositionChanged = topHalf !== this.state.topHalf;
+      vPositionChanged = topHalf !== this.state.topHalf || oneThird !== this.state.topThird || twoThird !== this.state.middleVThird || threeThird !== this.state.bottomThird;
       hPositionChanged = leftHalf !== this.state.leftHalf;
 
       // at this point, if our visibility is not what we expected,
       // update our state so that we can trigger whatever needs to
       // happen.
-      if (visibilityChanged  || vPositionChanged || hPositionChanged) {
+      if (visibilityChanged || vPositionChanged || hPositionChanged) {
         // set State first:
         this.setState({
             visible: visible,
             topHalf: topHalf,
             bottomHalf: !topHalf,
             leftHalf: leftHalf,
-            rightHalf: !leftHalf
+            rightHalf: !leftHalf,
+            topThird: oneThird,
+            middleVThird: twoThird,
+            bottomThird: threeThird
           },
           // then notify the component the value was changed:
           function () {
@@ -89,7 +97,7 @@
      * listening on, or with argument "true" to turn listening on and
      * immediately check whether this element is already visible or not.
      */
-    enableVisbilityHandling: function(checkNow) {
+    enableVisbilityHandling: function (checkNow) {
       if (typeof window === "undefined") {
         return console.error("This environment lacks 'window' support.");
       }
@@ -103,14 +111,14 @@
       }
       var domnode = this._dom_node;
 
-      this._rcv_fn = function() {
-        if(this._rcv_lock) {
+      this._rcv_fn = function () {
+        if (this._rcv_lock) {
           this._rcv_schedule = true;
           return;
         }
         this._rcv_lock = true;
         this.checkComponentVisibility();
-        this._rcv_timeout = setTimeout(function() {
+        this._rcv_timeout = setTimeout(function () {
           this._rcv_lock = false;
           if (this._rcv_schedule) {
             this._rcv_schedule = false;
@@ -138,7 +146,7 @@
      * and you only really need to do something once, like loading in
      * static assets on first-time-in-view-ness (that's a word, right?).
      */
-    disableVisbilityHandling: function() {
+    disableVisbilityHandling: function () {
       clearTimeout(this._rcv_timeout);
       if (this._rcv_fn) {
         var domnode = this._dom_node;
@@ -156,12 +164,12 @@
     }
   };
 
-  if(typeof module !== "undefined") {
+  if (typeof module !== "undefined") {
     module.exports = ComponentVisibilityMixin;
   }
 
   else if (typeof define !== "undefined") {
-    define(function() {
+    define(function () {
       return ComponentVisibilityMixin;
     });
   }
